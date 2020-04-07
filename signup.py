37b753a90ea38919487a210a3db438c2b669897b
Peter Evans <peter-evans@users.noreply.github.com>
2020-04-07 19:32:38 +0000

    autopep8 action fixes

--- signup.py
+++ signup.py
@@ -3,43 +3,51 @@ from firebaseUtil import get_fb_instance
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
 from user_management import createUserObject, parseUser, getUsers
-import json, time
+import json
+import time
 
 ph = PasswordHasher()
 
 signup_pages = Blueprint('signup_pages', __name__)
 
+
 @signup_pages.route('/signup', methods=['GET', 'POST'])
 def signup_page():
-	if(request.method == 'POST'):
-		if request.form.get('removeFlash') == None:
-			return signup(request.form['username'], request.form['password'], request.form['email'])
-		else:
-			return redirect(url_for("signup_pages.signup_page"))
-	elif(request.method == 'GET'):
-		return render_template("signup.html")
+    if(request.method == 'POST'):
+        if request.form.get('removeFlash') is None:
+            return signup(
+                request.form['username'],
+                request.form['password'],
+                request.form['email'])
+        else:
+            return redirect(url_for("signup_pages.signup_page"))
+    elif(request.method == 'GET'):
+        return render_template("signup.html")
 
-def signup(username : str, password : str, email : str):  
-	users = getUsers()
-	if(users != None):
-		if(get_fb_instance().child("delay").get() != None):
-			if(float(get_fb_instance().child("delay").get().val())+10 < float(time.time())):
-				get_fb_instance().update({"delay" : str(time.time())})
-			else:
-				flash("Too many users are signing up right now! Please try again later.")
-				return redirect(url_for("signup_pages.signup_page"))
-		else:
-			get_fb_instance().update({"delay" : str(time.time())})
-	if users != None and [x for x in users if x.key().lower() == username.lower() or parseUser(x).email.lower() == email.lower()]:
-		flash("Username or email already taken!", "error")
-		return redirect(url_for("signup_pages.signup_page"))
-	else:
-		try:
-			user=createUserObject(username, str(ph.hash(password)), 0, email)
-			get_fb_instance().child("users").update({username : json.dumps(user.__dict__)})
-			session['user_id'] = user.id
-			return redirect(url_for("index"))
-		except Exception as e:
-			print(e)
-			flash("An unknown error occured!", "error")
-			return redirect(url_for("signup_pages.signup_page"))
+
+def signup(username: str, password: str, email: str):
+    users = getUsers()
+    if(users is not None):
+        if(get_fb_instance().child("delay").get() is not None):
+            if(float(get_fb_instance().child("delay").get().val()) + 10 < float(time.time())):
+                get_fb_instance().update({"delay": str(time.time())})
+            else:
+                flash("Too many users are signing up right now! Please try again later.")
+                return redirect(url_for("signup_pages.signup_page"))
+        else:
+            get_fb_instance().update({"delay": str(time.time())})
+    if users is not None and [x for x in users if x.key().lower(
+    ) == username.lower() or parseUser(x).email.lower() == email.lower()]:
+        flash("Username or email already taken!", "error")
+        return redirect(url_for("signup_pages.signup_page"))
+    else:
+        try:
+            user = createUserObject(username, str(ph.hash(password)), 0, email)
+            get_fb_instance().child("users").update(
+                {username: json.dumps(user.__dict__)})
+            session['user_id'] = user.id
+            return redirect(url_for("index"))
+        except Exception as e:
+            print(e)
+            flash("An unknown error occured!", "error")
+            return redirect(url_for("signup_pages.signup_page"))
--- user_management.py
+++ user_management.py
@@ -5,8 +5,16 @@ import pyrebase
 from json import JSONDecodeError
 import time
 
+
 class User:
-    def __init__(self, id: str, username: str, password: str, role: int, email : str, notes : str):
+    def __init__(
+            self,
+            id: str,
+            username: str,
+            password: str,
+            role: int,
+            email: str,
+            notes: str):
         self.id = id
         self.username = username
         self.password = password
@@ -17,34 +25,51 @@ class User:
     def __repr__(self):
         return f'<User: {self.username}>'
 
+
 def createUser(username, password, role, email, *id):
     return createUserObject(username, password, role, email, *id).__dict__
-def parseUser(data): # Smarter implementation
+
+
+def parseUser(data):  # Smarter implementation
     if(isinstance(data, str)):
-        data=json.loads(data)
-        return User(data["id"], data["username"], data["password"], data["role"], data["email"], data["notes"])
+        data = json.loads(data)
+        return User(
+            data["id"],
+            data["username"],
+            data["password"],
+            data["role"],
+            data["email"],
+            data["notes"])
     elif(isinstance(data, User)):
         return data
-    elif(str(type(data) == "<class 'pyrebase.pyrebase.Pyre'>")):
+    elif(str(isinstance(data, "<class 'pyrebase.pyrebase.Pyre'>"))):
         try:
             return parseUser(data.val())
         except AttributeError as e:
             print(str(e) + " at line 32 in user_management")
     else:
-        raise(TypeError(str(type(data)) + " was given expected type JSON or PyreBase"))
+        raise(TypeError(str(type(data)) +
+                        " was given expected type JSON or PyreBase"))
+
+
 def createUserObject(username, password, role, email, *id):
-    ID=createID()
+    ID = createID()
     if(len(id) == 0):
         return User(ID, username, password, role, email, "")
     else:
         return User(id[0], username, password, role, email, "")
+
+
 def getUsers():
     return get_fb_instance().child("users").get().each()
+
+
 def deleteUser(data, *method):
     try:
         users = getUsers()
         if(len(method) == 0 or method[0] == "username"):
-            user = [x for x in users if parseUser(x).username == data or parseUser(x).email == data][0]
+            user = [x for x in users if parseUser(
+                x).username == data or parseUser(x).email == data][0]
             get_fb_instance().child("users").child(parseUser(user).username).remove()
             get_fb_instance().child("notes").child(parseUser(user).id).remove()
             return "User succesfuly deleted"
@@ -55,38 +80,51 @@ def deleteUser(data, *method):
             return "User succesfuly deleted"
     except IndexError:
         return "Error: User not found"
+
+
 def getUser(id):
     try:
         users = get_fb_instance().child("users").get().each()
         return parseUser([x for x in users if parseUser(x).id == id][0])
     except IndexError:
         return None
+
+
 def getRawUser(id):
     users = getUsers()
     return [x for x in users if parseUser(x).id == id][0]
+
+
 def updateUser(id, user_object):
     user = getUser(id)
-    get_fb_instance().child("users").update({user.username : json.dumps(user_object.__dict__)})
+    get_fb_instance().child("users").update(
+        {user.username: json.dumps(user_object.__dict__)})
+
+
 def updateChild(key, child, data):
-    get_fb_instance().child(child).update({key : json.dumps(data)})
-def createID(): # Fix session hangaround with deleted ID and jump to new users bug.
-    if(getUsers() != None):
-        ID=-1
-        if(getUsers() != None): 
-            ID=len(getUsers())
-        taken="1"
+    get_fb_instance().child(child).update({key: json.dumps(data)})
+
+
+def createID():  # Fix session hangaround with deleted ID and jump to new users bug.
+    if(getUsers() is not None):
+        ID = -1
+        if(getUsers() is not None):
+            ID = len(getUsers())
+        taken = "1"
         while len(taken) > 0:
-            ID+=1
+            ID += 1
             taken = [x for x in getUsers() if parseUser(x).id == ID]
         return ID
     else:
         return 0
+
+
 def getNotes(id):
     try:
         notes = get_fb_instance().child("notes").get().each()
-        if(notes != None):
+        if(notes is not None):
             note = [x for x in notes if str(x.key()) == str(id)][0]
-            if(note.val() != None):
+            if(note.val() is not None):
                 return note.val()[1:-1]
             else:
                 return ""
@@ -94,9 +132,12 @@ def getNotes(id):
             return ""
     except IndexError:
         return ""
+
+
 def userExists(session):
-    if(session.get('user_id') != None):
+    if(session.get('user_id') is not None):
         user_id = session.get('user_id')
         users = getUsers()
-        exists = len([x for x in users if str(parseUser(x).id) == str(user_id)]) > 0
+        exists = len([x for x in users if str(
+            parseUser(x).id) == str(user_id)]) > 0
         return exists
