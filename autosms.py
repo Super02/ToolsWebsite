f20de2ef56742ea63d2271ea29701456169dda39
Peter Evans <peter-evans@users.noreply.github.com>
2020-05-03 16:01:21 +0000

    autopep8 action fixes

--- autosms.py
+++ autosms.py
@@ -4,14 +4,20 @@ from user_management import getUserSmSes, updateRawChild, get_fb_instance
 
 autosms = Blueprint('autosms', __name__)
 
+
 @autosms.route('/autosms', methods=['GET', 'POST'])
 def auto_sms():
     smses = getUserSmSes(session.get("user_id"))
-    if(smses != None and smses > 0):
+    if(smses is not None and smses > 0):
         if request.method == "POST":
             for x in ["repeat", "message", "date", "dst", "src"]:
-                updateRawChild(x, "smses/{}/pending".format(session.get("user_id")), request.form.get(x))
-        if(smses != None and smses > 0):
+                updateRawChild(x,
+                               "smses/{}/pending".format(session.get("user_id")),
+                               request.form.get(x))
+        if(smses is not None and smses > 0):
             return render_template('autosms.html')
     else:
-        return render_template("showtext.html", title="403", text="Permission denied.")
+        return render_template(
+            "showtext.html",
+            title="403",
+            text="Permission denied.")
