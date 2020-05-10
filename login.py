b40043f1c9b0297e7677aa21915bc860f896441a
Peter Evans <peter-evans@users.noreply.github.com>
2020-05-10 20:35:00 +0000

    autopep8 action fixes

--- login.py
+++ login.py
@@ -3,7 +3,12 @@ from firebaseUtil import get_fb_instance
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
 from user_management import parseUser, getUsers, updateRawChild, updateUser, sendEmailToUser
-import json, uuid, time, re, requests, os
+import json
+import uuid
+import time
+import re
+import requests
+import os
 
 ph = PasswordHasher()
 
@@ -13,10 +18,13 @@ cooldown = time.time()
 
 login_pages = Blueprint('login_pages', __name__)
 
+
 @login_pages.route('/forgot', methods=['GET', 'POST'])
 def forgot():
     if(request.method == 'POST'):
-        data = {'secret': H_SECRET_KEY, 'response': request.form['h-captcha-response']}
+        data = {
+            'secret': H_SECRET_KEY,
+            'response': request.form['h-captcha-response']}
         response = requests.post(url=H_VERIFY_URL, data=data)
         success = response.json()['success']
         if(not success):
@@ -35,19 +43,30 @@ def sendResetEmail(email):
     user = [x for x in users if str(parseUser(x).email) == str(email)]
     epoch = time.time()
     if(len(user) > 0):
-        user=parseUser(user[0])
-        UUID = str(uuid.uuid3(uuid.NAMESPACE_DNS, email)) + str(uuid.uuid4()) + "--" + str(epoch+900)
+        user = parseUser(user[0])
+        UUID = str(uuid.uuid3(uuid.NAMESPACE_DNS, email)) + \
+            str(uuid.uuid4()) + "--" + str(epoch + 900)
         updateRawChild(user.id, "userdata/reset", UUID)
-        f = open("templates/resetemailResponse.html", "r") 
-        sendEmailToUser(user.id, "Password reset", f.read().replace("{USER}", user.username).replace("{LINK}", request.url_root + "reset/" + UUID))
-    
+        f = open("templates/resetemailResponse.html", "r")
+        sendEmailToUser(
+            user.id,
+            "Password reset",
+            f.read().replace(
+                "{USER}",
+                user.username).replace(
+                "{LINK}",
+                request.url_root +
+                "reset/" +
+                UUID))
+
 
 @login_pages.route('/reset/<token>', methods=['POST', 'GET'])
 def reset_user(token):
-    if(get_fb_instance().child("userdata/reset").get().each() == None):
+    if(get_fb_instance().child("userdata/reset").get().each() is None):
         abort(403, "Token expired or does not exist")
     if(request.method == 'POST'):
-        user = [x for x in get_fb_instance().child("userdata/reset").get().each() if x.val() == str(token)]
+        user = [x for x in get_fb_instance().child(
+            "userdata/reset").get().each() if x.val() == str(token)]
         try:
             epoch = user[0].val().split("--")[1]
         except(IndexError):
@@ -55,7 +74,8 @@ def reset_user(token):
             return redirect(url_for("login_pages.login_page"))
         now = time.time()
         if(len(user) > 0 and float(epoch) > now):
-            user = parseUser([x for x in getUsers() if parseUser(x).id == user[0].key()][0])
+            user = parseUser(
+                [x for x in getUsers() if parseUser(x).id == user[0].key()][0])
             password = request.form.get("password")
             if(not re.match(r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,200}$", password)):
                 flash("Invalid password.", "error")
@@ -69,12 +89,13 @@ def reset_user(token):
             flash("Error! Token expired or does not exist.", "error")
             return redirect(url_for("login_pages.login_page"))
     return render_template('reset.html')
-    
+
 
 @login_pages.route('/login', methods=['GET', 'POST'])
 def login_page():
     if(request.method == 'POST'):
-        if request.form.get('removeFlash') is None or request.form.get('removeFlash') == '':
+        if request.form.get('removeFlash') is None or request.form.get(
+                'removeFlash') == '':
             return login(
                 request.form['username_email'],
                 request.form['password'])
